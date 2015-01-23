862bb0b4bb88c568d58d5bbe275d5641ad3fbe20
Yakup Korkmazoglu <ymemsal@gmail.com>
2015-01-23 15:36:52 -0800

    first init

---
+++ ResideMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ResideMenu
+//
+//  Created by Yakup Korkmazoglu on 1/23/15.
+//  Copyright (c) 2015 Yakup Korkmazoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ResideMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ResideMenu
+//
+//  Created by Yakup Korkmazoglu on 1/23/15.
+//  Copyright (c) 2015 Yakup Korkmazoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
